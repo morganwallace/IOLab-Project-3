3352e4259f661c7193868ec77294326b586e40bc
Morgan <Morgan@Morgans-MacBook-Pro.local>
2012-11-10 12:48:46 -0800

    Google Results.py returns JSON data with article urls to data2.js

--- GoogleResults.py
+++ GoogleResults.py
@@ -1,25 +1,69 @@
+##This code will query Google for Presidential Debate related newspaper articles and return URLs for the top hits.
+#Results are saved in JSON format in 'data.js'
+
+#Program written by Morgan Wallace
+
 #!/usr/bin/python3
 import json
 import urllib.request, urllib.parse
-
-def showsome(searchfor):
-  query = urllib.parse.urlencode({'q': searchfor})
-  url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
-  search_response = urllib.request.urlopen(url)
-  search_results = search_response.read().decode("utf8")
-  results = json.loads(search_results)
-  data = results['responseData']
-  print('Total results: %s' % data['cursor']['estimatedResultCount'])
-  hits = data['results']
-  print('Top %d hits:' % len(hits))
-  for h in hits: print(' ', h['url'])
-  print('For more results, see %s' % data['cursor']['moreResultsUrl'])
+from time import sleep
 
 
-newspapers = ['sfgate.com','nytimes.com','latimes.com','washingtonpost.com']
-debateDates = ['October 3','October 16','October 22']
+def GoogleSearch(searchfor):
+    query = urllib.parse.urlencode({'q': searchfor})
+    url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
+    search_response = urllib.request.urlopen(url)
+    search_results = search_response.read().decode("utf8")
+    results = json.loads(search_results)
+    data = results['responseData']
+    print("Searching Google for: " + searchfor)
+    try:
+        hits = data['results']
+        urls = []
+        for h in hits: urls.append(h['url'])
+        return urls[:2] #return the first 2 links
+    except: return []
 
+jsonFile = "data2.js"
+
+#counts the lines in the 'jsonFile' file so the JSON variable can have the correct ID
+def countInFile():
+    file =  open(jsonFile,"r")
+    counter = 0
+    for line in file: counter +=1
+    return counter
+
+#Export to 'data.js' for sentiment analysis
+def saveJSON(JSONdata):
+    jsFile = open(jsonFile,'a')
+    jsFile.write(JSONdata+'\n')
+    jsFile.close()
+
+##############
+    #modify this information for queries.
+newspapers = ['latimes.com','washingtonpost.com']
+debateDates = ['October 16','October 17','October 22','October 23']
+#############
+
+months = {'October':"10","November":"11"} #used later to reformat the date
+jsonNo = countInFile()+1 #initiates a new ID for JSON entries.
+
+print("The following has been appended to 'data.js':\n")
 for newspaper in newspapers:
     for date in debateDates:
-        showsome(newspaper + 'obama romney presidential debate' + date)
+        query = newspaper + ' Obama Romney Presidential debate ' + date
+        urls = GoogleSearch(query)
+        if urls: #if Google search results were returned
+            spaceLoc = date.find(" ")
+            date = int("2012"+months[date[:spaceLoc]]+date[spaceLoc+1:])
+            debateNo = "1"
+            if  20121014 < date <20121021: debateNo = "2"
+            elif date >= 20121021: debateNo = "3"           
+            for url in urls:
+                jsonName = "var s"+str(jsonNo)+" = {'publication':'"+newspaper+"','date':'"+str(date)+"','url':'"+url+"','debate':'"+debateNo+"'};"
+                saveJSON(jsonName)
+                print(jsonName)
+                jsonNo +=1
+                sleep(2)
+        else: print("No URLs were found for the query: "+ query)
 
---
+++ data2.js
@@ -0,0 +1,6 @@
+var s1 = {'publication':'latimes.com','date':'2012103','url':'http://www.latimes.com/news/politics/la-pn-live-stream-presidential-debate-20121022,0,3936767.story','debate':'1'};
+var s2 = {'publication':'latimes.com','date':'2012103','url':'http://www.latimes.com/news/politics/la-pn-2012-presidential-debate-20121022,0,7479154.story','debate':'1'};
+var s3 = {'publication':'latimes.com','date':'2012104','url':'http://www.latimes.com/news/politics/la-pn-2012-presidential-debate-20121022,0,7479154.story','debate':'1'};
+var s4 = {'publication':'latimes.com','date':'2012104','url':'http://www.latimes.com/news/opinion/opinion-la/la-ol-presidential-debate-florida-obama-romney-20121019,0,1998778.story','debate':'1'};
+var s5 = {'publication':'latimes.com','date':'20121017','url':'http://www.latimes.com/news/politics/la-pn-presidential-debate-taxes-deficit-20121016,0,6300634.story','debate':'2'};
+var s6 = {'publication':'latimes.com','date':'20121017','url':'http://www.latimes.com/news/opinion/opinion-la/la-ol-presidential-debate-obama-quadry-20121015,0,3281102.story','debate':'2'};
